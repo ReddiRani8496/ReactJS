e14dbf0d13386e30240ca94943dce438e647c057
reddirani8496 <reddiranibattuluri@gmail.com>
2024-06-03 23:26:50 +0530

    jsx components uploade..

--- Day-2/jsxcomponents/src/index.js
+++ Day-2/jsxcomponents/src/index.js
@@ -6,12 +6,17 @@ import App from "./App";
 const color = "red";
 const size = 50;
 
-const name = <h1>Rahul</h1>;
+const name = <h1 id="yellowgreen">Rahul</h1>;
 const age = (
   <p style={{ textAlign: "center", fontSize: size + "px", color: color }}>23</p>
 );
 const email = <p>rahul@gmail.com</p>;
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+let element = document.createElement("div"); // creating div element using javascript
+element.id = "root";
+document.body.appendChild(element);
+const root = ReactDOM.createRoot(element);
 root.render(
   <React.StrictMode>
     <App />

062e5aa334d9a1cd51fd8d9a0e850f9a54ef5f0e
reddirani8496 <reddiranibattuluri@gmail.com>
2024-06-03 23:20:32 +0530

    Jsx components uploaded..

---
+++ Day-2/jsxcomponents/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const color = "red";
+const size = 50;
+
+const name = <h1>Rahul</h1>;
+const age = (
+  <p style={{ textAlign: "center", fontSize: size + "px", color: color }}>23</p>
+);
+const email = <p>rahul@gmail.com</p>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    <div>
+      {name}
+      {age}
+      {email}
+    </div>
+  </React.StrictMode>
+);

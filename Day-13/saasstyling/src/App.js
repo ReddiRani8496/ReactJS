060a37eecc6d48f50c62c3b10d66b9bbe624210c
reddirani8496 <reddiranibattuluri@gmail.com>
2024-06-14 00:41:03 +0530

    saas styling program uploaded..

---
+++ Day-13/saasstyling/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "./saas.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to xAble Life</h1>
+      <h2>Courses Available: </h2>
+      <h3>Java</h3>
+      <h4>Python</h4>
+    </div>
+  );
+}
+
+export default App;

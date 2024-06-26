eb8897026d3cde45be172b53ab9ec0f7321a130b
reddirani8496 <reddiranibattuluri@gmail.com>
2024-06-26 22:17:39 +0530

    routing uploaded..

---
+++ Day-20/nested-routes/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="./home"></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ Day-20/nested-routes/src/home.js
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
+function Home() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="./home"></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default Home;

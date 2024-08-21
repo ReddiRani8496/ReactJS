4d10880c1508da79612ae3b90ee49a95da0962c7
reddirani8496 <reddiranibattuluri@gmail.com>
2024-08-21 23:19:20 +0530

    Shimmer component updated

--- Day-35/food_villa_app/src/components/HeaderComponent.js
+++ Day-35/food_villa_app/src/components/HeaderComponent.js
@@ -1,26 +1,37 @@
 import logo from "./logo.jpg";
+import React, { useState } from "react";
 const Title = () => (
   <a href="/">
     <img className="logo" src={logo} alt="logo" />
   </a>
 );
 
-const NavItems = () => (
-  <div className="nav-items">
-    <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-      <li>Cart</li>
-    </ul>
-  </div>
-);
+const NavItems = () => {
+  return (
+    <div className="nav-items">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  );
+};
 
-const Header = () => (
-  <div className="header">
-    <Title />
-    <NavItems />
-  </div>
-);
+const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  return (
+    <div className="header">
+      <Title />
+      <NavItems />
+      {isLoggedIn ? (
+        <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+      ) : (
+        <button onClick={() => setIsLoggedIn(true)}>Login</button>
+      )}
+    </div>
+  );
+};
 
 export default Header;
--- Day-35/food_villa_app/src/components/Shimmer.js
+++ Day-35/food_villa_app/src/components/Shimmer.js
@@ -1,5 +1,18 @@
 const Shimmer = () => {
-  return <h1>Shimmer UI Loading........</h1>;
+  return (
+    <div className="shimmer-container">
+      {Array(10)
+        .fill("")
+        .map((_, index) => (
+          <div key={index} className="shimmer-card">
+            <div className="shimmer-img"></div>
+            <div className="shimmer-text shimmer-text-short"></div>
+            <div className="shimmer-text shimmer-text-long"></div>
+            <div className="shimmer-text shimmer-text-medium"></div>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default Shimmer;

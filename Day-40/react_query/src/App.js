fb56108a0d68f983baab7c7eb0b43984916e6b14
reddirani8496 <reddiranibattuluri@gmail.com>
2024-09-06 08:47:15 +0530

    use-mutation method added

--- Day-40/react_query/src/App.js
+++ Day-40/react_query/src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import { useQuery, useMutation } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import "./App.css";
 import { query } from "express";
 
@@ -18,15 +18,46 @@ function App() {
   //   queryKey: ["posts"],
   //   queryFn: () => wait(1000).then(() => [...posts]),
   // });
+  const queryClient = useQueryClient();
   const postsQuery = useQuery({
     queryKey: ["posts"],
     queryFn: () => Promise.reject("Error in loading posts"),
   });
 
+  const newMutationPost = useMutation({
+    mutationFn: (title) => {
+      return wait(1000).then(() => ({ id: crypto.randomUUID(), title }));
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries("posts");
+    },
+  });
+
   if (postsQuery.isLoading) return <h1>Loading.....</h1>;
 
   if (postsQuery.isError) return <h1>{JSON.stringify(postsQuery.error)}</h1>;
-  return <div className="App">React Query</div>;
+  return (
+    <div className="App">
+      {postsQuery.data.map((post) => (
+        <p id={post.id}>{post.title}</p>
+      ))}
+      <button
+        disabled={newMutationPost.isLoading}
+        onClick={() => newMutationPost.mutate("New Post")}
+      ></button>
+    </div>
+  );
 }
 
 export default App;
+
+/**
+ *  /posts ["post"]
+ *
+ *  /posts/1 ["posts",1]
+ *
+ *  /posts?authorId=1 ["posts", {authorId: 1}]
+ *
+ *  /posts/2/comments  ["posts", 2, "comments"]
+ */

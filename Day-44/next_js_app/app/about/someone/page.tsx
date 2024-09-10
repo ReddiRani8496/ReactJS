95e6d4810a59dee325053bca98e2cddd7a87c24a
reddirani8496 <reddiranibattuluri@gmail.com>
2024-09-10 22:34:40 +0530

    next-js routing program uploaded

---
+++ Day-44/next_js_app/app/about/someone/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AboutSomeone() {
+  return <div>This is About Someone Page</div>;
+}
+
+export default AboutSomeone;
---
+++ Day-44/next_js_app/app/about/us/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AboutUs() {
+  return <div>This is AboutUs page</div>;
+}
+
+export default AboutUs;

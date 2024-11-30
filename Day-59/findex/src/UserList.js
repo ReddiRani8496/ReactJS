2101f48075c023e59fe9228c231f290599aab785
reddirani8496 <reddiranibattuluri@gmail.com>
2024-11-30 20:33:56 +0530

    Find method example program uploaded

---
+++ Day-59/findex/src/UserList.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const UserList = () => {
+  const [users, setUsers] = useState([
+    { id: 1, name: "Rahul" },
+    { id: 2, name: "Leela" },
+    { id: 3, name: "Rama" },
+  ]);
+
+  const [foundUser, setFoundUser] = useState(null);
+
+  const findUserByName = (name) => {
+    const user = users.find(
+      (user) => user.name.toLowerCase() === name.toLowerCase()
+    );
+    setFoundUser(user);
+  };
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+
+      <button onClick={() => findUserByName("Leela")}>Find Leela</button>
+
+      {foundUser ? <p>Found User: {foundUser.name}</p> : <p>No user found</p>}
+    </div>
+  );
+};
+
+export default UserList;

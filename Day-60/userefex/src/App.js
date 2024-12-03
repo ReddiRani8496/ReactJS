c36eb8ebdba8cc9d15ce76b7058ce98050f06831
reddirani8496 <reddiranibattuluri@gmail.com>
2024-12-03 14:43:52 +0530

    useref example uploaded

---
+++ Day-60/userefex/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef } from "react";
+
+function App() {
+  const inputElement = useRef();
+
+  const focusInput = () => {
+    inputElement.current.focus();
+  };
+
+  return (
+    <>
+      <input type="text" ref={inputElement} />
+      <button onClick={focusInput}>Focus Input</button>
+    </>
+  );
+}
+
+export default App;

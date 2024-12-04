355432aad0c9545ac47626219877ef5ba87ac2e2
reddirani8496 <reddiranibattuluri@gmail.com>
2024-12-04 21:03:18 +0530

    test cases added

--- Day-60/userefex/src/App.test.js
+++ Day-60/userefex/src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("it should focus input when button is clicked", () => {
+  render(<App />);
+
+  const button = screen.getByText(/focus input/i);
+  fireEvent.click(button);
+
+  const input = screen.getByRole("textbox");
+  expect(input).toHaveFocus();
 });

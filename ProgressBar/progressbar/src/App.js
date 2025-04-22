107acd75a23c93810a87d021b88eba7f3c9351fe
reddirani8496 <reddiranibattuluri@gmail.com>
2025-04-22 22:04:17 +0530

    Progress bar component uploaded

--- ProgressBar/progressbar/src/App.js
+++ ProgressBar/progressbar/src/App.js
@@ -3,10 +3,13 @@ import "./App.css";
 import ProgressBar from "./Components/ProgressBar";
 
 function App() {
+  const bars = [0, 5, 10, 30, 50, 70, 90, 100];
   return (
     <div className="App">
       <h1>Progress Bar</h1>
-      <ProgressBar progress={50} />
+      {bars.map((value) => (
+        <ProgressBar progress={value} key={value} />
+      ))}
     </div>
   );
 }
--- ProgressBar/progressbar/src/Components/ProgressBar.jsx
+++ ProgressBar/progressbar/src/Components/ProgressBar.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./styles.css";
 
 function ProgressBar({ progress }) {
+  const [animatedProgress, setAnimatedProgress] = React.useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimatedProgress(progress);
+    }, 100);
+  }, [progress]);
   return (
     <div className="outer">
-      <div className="inner" style={{ width: `${progress}%` }}>
+      <div
+        className="inner"
+        style={{
+          transform: `translateX(-${100 - animatedProgress}%)`,
+          color: animatedProgress >= 5 ? "white" : "black",
+        }}
+        role="progressbar"
+        aria-valuenow={animatedProgress}
+        aria-valuemin="0"
+        aria-valuemax="100"
+      >
         {progress}%
       </div>
     </div>

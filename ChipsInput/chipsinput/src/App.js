d60d06d16739b9b2ac2908a7730479edeb7ab979
reddirani8496 <reddiranibattuluri@gmail.com>
2025-05-01 22:09:28 +0530

    Chips component uploaded

--- ChipsInput/chipsinput/src/App.js
+++ ChipsInput/chipsinput/src/App.js
@@ -6,8 +6,18 @@ function App() {
   const [inputText, setInputText] = useState("");
   const [chips, setChips] = useState([]);
   const handleKeyDown = (e) => {
-    if (e.key == "enter") {
+    if (e.key === "Enter") {
+      if (inputText.trim() !== "") {
+        setChips((prevChips) => [...prevChips, inputText]);
+        setInputText("");
+      }
     }
+  };
+  const handleDeleteChip = (index) => {
+    // setChips((prevChips) => prevChips.filter((_, i) => i !== index));
+    const newChips = [...chips];
+    newChips.splice(index, 1);
+    setChips(newChips);
   };
   return (
     <div className="App">
@@ -19,6 +29,33 @@ function App() {
         onChange={(e) => setInputText(e.target.value)}
         onKeyDown={(e) => handleKeyDown(e)}
       />
+      <div style={{ display: "flex", flexWrap: "wrap", marginTop: "10px" }}>
+        {chips.map((chip, index) => (
+          <div
+            key={index}
+            style={{
+              backgroundColor: "#007bff",
+              color: "white",
+              padding: "5px 10px",
+              borderRadius: "20px",
+              margin: "5px",
+            }}
+          >
+            {chip}
+            <button
+              style={{
+                color: "red",
+                marginLeft: "10px",
+                border: "none",
+                background: "none",
+              }}
+              onClick={() => handleDeleteChip(index)}
+            >
+              X
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

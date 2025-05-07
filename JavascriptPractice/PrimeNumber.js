211f8faff5844fe4f9dc321ca4ab6df6ec9273e7
reddirani8496 <reddiranibattuluri@gmail.com>
2025-05-07 22:23:42 +0530

    Javascript programs uploaded

---
+++ JavascriptPractice/PrimeNumber.js
@@ -0,0 +1,9 @@
+const primeNumber = (num) => {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+console.log(primeNumber(5)); // true
+console.log(primeNumber(10)); // false

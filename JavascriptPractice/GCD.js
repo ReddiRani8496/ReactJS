f8a277d7271d53fa7739f2ab89fa5f1b0eb5b5c2
reddirani8496 <reddiranibattuluri@gmail.com>
2025-05-08 23:00:07 +0530

    Added lcm and gcd programs

--- JavascriptPractice/GCD.js
+++ JavascriptPractice/GCD.js
@@ -11,3 +11,14 @@ const GCD = (a, b) => {
 };
 console.log(GCD(12, 15)); // 3
 console.log(GCD(100, 25)); // 25
+
+//approach 2
+const GCD1 = (num1, num2) => {
+  while (num1 != num2) {
+    if (num1 > num2) num1 = num1 - num2;
+    else num2 = num2 - num1;
+  }
+  return num1;
+};
+console.log(GCD1(12, 15)); // 3
+console.log(GCD1(100, 25)); // 25
--- JavascriptPractice/LCM.js
+++ JavascriptPractice/LCM.js
@@ -9,3 +9,18 @@ const lcm = (a, b) => {
 };
 console.log(lcm(4, 6)); // 12
 console.log(lcm(15, 20)); // 60
+
+// approach 2
+const lcm1 = (a, b) => {
+  const gcd = (num1, num2) => {
+    while (num1 != num2) {
+      if (num1 > num2) num1 = num1 - num2;
+      else num2 = num2 - num1;
+    }
+    return num1;
+  };
+  return (a * b) / gcd(a, b);
+};
+
+console.log(lcm1(4, 6)); // 12
+console.log(lcm1(15, 20)); // 60

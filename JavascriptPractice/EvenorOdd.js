edeeaef62d76fb8596f9b20ad42eac5b194c69a7
reddirani8496 <reddiranibattuluri@gmail.com>
2025-05-09 22:12:57 +0530

    Added lcm and gcd programs

--- JavascriptPractice/EvenorOdd.js
+++ JavascriptPractice/EvenorOdd.js
@@ -6,3 +6,15 @@ const evenOrOdd = (num) => {
 };
 console.log(evenOrOdd(4)); // 4 is even
 console.log(evenOrOdd(5)); // 5 is odd
+
+// approach 2
+const evenOrOdd1 = (num) => {
+  return num % 2 === 0 ? `${num} is even` : `${num} is odd`;
+};
+console.log(evenOrOdd1(4)); // 4 is even
+
+// approach 3
+const evenOrOdd2 = (num) => {
+  return num & 1 ? `${num} is odd` : `${num} is even`;
+};
+console.log(evenOrOdd2(4)); // 4 is even

ce1bb0be72d74da519cdca1a366f11e114e7f98d
reddirani8496 <reddiranibattuluri@gmail.com>
2025-05-10 22:10:41 +0530

    Added lcm and gcd programs

--- JavascriptPractice/Factorial.js
+++ JavascriptPractice/Factorial.js
@@ -7,3 +7,12 @@ const factorial = (num) => {
   return fact;
 };
 console.log("factorial of 3 ", factorial(3));
+
+// approach-2
+const factorial1 = (num) => {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  return num * factorial1(num - 1);
+};
+console.log("factorial of 3 ", factorial1(3));

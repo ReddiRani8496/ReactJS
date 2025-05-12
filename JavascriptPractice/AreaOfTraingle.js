114be7d527e3151076ddff16625d6ef834754876
reddirani8496 <reddiranibattuluri@gmail.com>
2025-05-12 16:37:47 +0530

    javascript program uploaded

--- JavascriptPractice/AreaOfTraingle.js
+++ JavascriptPractice/AreaOfTraingle.js
@@ -2,3 +2,10 @@ const areaOfTraingle = (length, breadth) => {
   return (length * breadth) / 2;
 };
 console.log("Area of Traingle ", areaOfTraingle(2, 3)); // 3
+
+//approach-2
+const areaOfTraingle1 = (length, breadth) => {
+  return 0.5 * length * breadth;
+};
+
+console.log("Area of Traingle ", areaOfTraingle1(2, 3)); // 3

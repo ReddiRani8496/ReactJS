20f72c159a3ebe62a27a98071ef5d97a4b471499
reddirani8496 <reddiranibattuluri@gmail.com>
2025-05-14 20:59:52 +0530

    prime number program uploaded

--- JavascriptPractice/PrimeNumber.js
+++ JavascriptPractice/PrimeNumber.js
@@ -7,3 +7,17 @@ const primeNumber = (num) => {
 };
 console.log(primeNumber(5)); // true
 console.log(primeNumber(10)); // false
+
+// Approach-2
+const isPrime = (num) => {
+  if (num <= 1) return false;
+  if (num === 2) return true;
+  if (num % 2 === 0) return false;
+
+  for (let i = 3; i <= Math.sqrt(num); i += 2) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+console.log(isPrime(5)); // true
+console.log(isPrime(10)); // false

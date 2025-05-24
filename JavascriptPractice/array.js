7cf6fc7b43a8bbd3acdc2eb108017ea16f7ce920
reddirani8496 <reddiranibattuluri@gmail.com>
2025-05-24 18:48:34 +0530

    min and max program uploaded

---
+++ JavascriptPractice/array.js
@@ -0,0 +1,7 @@
+const arr = [12, 5, 8, 130, 44];
+
+const max = Math.max(...arr);
+const min = Math.min(...arr);
+
+console.log("Maximum:", max); // Output: Maximum: 130
+console.log("Minimum:", min); // Output: Minimum: 5

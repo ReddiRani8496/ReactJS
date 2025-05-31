7a57bb53befcf15fe69e0edb6ee718bf13f3ad49
reddirani8496 <reddiranibattuluri@gmail.com>
2025-05-31 18:22:19 +0530

    move zeros to end of the array program uploaded

--- JavascriptPractice/MoveZerosToEndOfZero.js
+++ JavascriptPractice/MoveZerosToEndOfZero.js
@@ -17,3 +17,28 @@ const moveZerosToEnd = (arr) => {
   return arr;
 };
 console.log(moveZerosToEnd(arr)); // Output: [1, 3, 12, 0, 0]
+
+// approach -2
+
+let arr1 = [0, 1, 0, 3, 12];
+const moveZerosToEnd1 = (arr) => {
+  let zeroCount = 0; // Count of zeros
+
+  // Traverse the array and count zeros
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      zeroCount++;
+    } else if (zeroCount > 0) {
+      // If we encounter a non-zero element after zeros, shift it to the left
+      arr[i - zeroCount] = arr[i];
+    }
+  }
+
+  // Fill the end of the array with zeros
+  for (let i = arr.length - zeroCount; i < arr.length; i++) {
+    arr[i] = 0;
+  }
+
+  return arr;
+};
+console.log(moveZerosToEnd1(arr1)); // Output: [1, 3, 12, 0, 0]

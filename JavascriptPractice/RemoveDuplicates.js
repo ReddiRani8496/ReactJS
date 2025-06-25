fcf88a808b5dce6a35b14315f51a867f5e082ee2
reddirani8496 <reddiranibattuluri@gmail.com>
2025-06-25 21:56:31 +0530

    program added

---
+++ JavascriptPractice/RemoveDuplicates.js
@@ -0,0 +1,15 @@
+const arr = [1, 2, 3, 4, 1, 5, 6, 4, 7, 8, 9, 10];
+const removeDuplicates = (arr) => {
+  const uniqueElements = [];
+  const seen = new Set();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!seen.has(arr[i])) {
+      uniqueElements.push(arr[i]);
+      seen.add(arr[i]);
+    }
+  }
+
+  return uniqueElements;
+};
+console.log("Array after removing duplicates:", removeDuplicates(arr)); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]

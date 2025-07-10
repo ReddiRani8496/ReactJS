cf5c91262cba29f52a66095468d7a6415d6aaa24
reddirani8496 <reddiranibattuluri@gmail.com>
2025-07-10 21:21:49 +0530

    Custom greeting program uploaded

---
+++ InterviewPractice/CustomGreeting.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+export default function Greeting() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const hour = time.getHours();
+  const getWishes = () => {
+    if (hour >= 5 && hour < 12) {
+      return "Good Morning!";
+    } else if (hour >= 12 && hour < 17) {
+      return "Good Afternoon!";
+    } else if (hour >= 17 && hour < 21) {
+      return "Good Evening!";
+    } else {
+      return "Good Night!";
+    }
+  };
+
+  return (
+    <div
+      className="modal-content"
+      style={{
+        textAlign: "center",
+        padding: "50px",
+        fontFamily: "Arial, sans-serif",
+        backgroundColor: "#f5f5f5",
+        color: "#333",
+      }}
+    >
+      <p>{getWishes()}</p>
+      <p>{time.toLocaleTimeString()}</p>
+    </div>
+  );
+}

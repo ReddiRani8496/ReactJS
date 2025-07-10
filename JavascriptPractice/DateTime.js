a91bad4653a67890baa7225cdd57a5b23ba2313c
reddirani8496 <reddiranibattuluri@gmail.com>
2025-07-10 21:20:53 +0530

    javascript date time program uploaded

---
+++ JavascriptPractice/DateTime.js
@@ -0,0 +1,10 @@
+const date = new Date(); // ISO 8601 format
+console.log("Current Date:", date);
+console.log(date.getFullYear());
+console.log(date.getMonth() + 1);
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+console.log(date.getTime());

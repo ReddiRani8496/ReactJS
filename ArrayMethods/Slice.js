482f6a2fa7613c3ea69036a3374a9137619a79d7
reddirani8496 <reddiranibattuluri@gmail.com>
2025-07-28 20:13:24 +0530

    slice method uploaded

---
+++ ArrayMethods/Slice.js
@@ -0,0 +1,5 @@
+const fuits = ["apple", "banana", "mango"];
+
+const slicedFruits = fuits.slice(1, 3); // it will return part of the array
+console.log(slicedFruits); // Output: ["banana", "mango"]
+console.log(fuits);

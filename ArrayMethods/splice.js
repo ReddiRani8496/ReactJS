7e9a1aeece7476bfcb598399288ebb5556136139
reddirani8496 <reddiranibattuluri@gmail.com>
2025-08-15 21:17:47 +0530

    splice method uploaded

---
+++ ArrayMethods/splice.js
@@ -0,0 +1,4 @@
+let fruits = ["apple", "banana", "cherry", "date"];
+let removedFruits = fruits.splice(1, 2);
+console.log(removedFruits); // Output: ["banana", "cherry"]
+console.log(fruits); // Output: ["apple", "date"]

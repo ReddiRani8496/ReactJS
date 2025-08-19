5054db11f89ffc2eac12ec0c72e99c9ea3ade3e8
reddirani8496 <reddiranibattuluri@gmail.com>
2025-08-19 22:39:19 +0530

    reduce program uploaded

--- ArrayMethods/reduce.js
+++ ArrayMethods/reduce.js
@@ -4,3 +4,11 @@ const sum = arr.reduce((accumulator, currentValue) => {
 });
 console.log(arr); // Output: [1, 2, 3, 5, 6]
 console.log(sum); // Output: 21
+
+// approach 2
+let arr2 = [1, 2, 3, 4, 5, 6];
+const sum2 = arr2.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0);
+console.log(arr2); // Output: [1, 2, 3, 4, 5, 6]
+console.log(sum2); // Output: 21

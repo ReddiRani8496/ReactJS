96717982967156873acc7a77606df5e6e5fe2240
reddirani8496 <reddiranibattuluri@gmail.com>
2025-08-22 22:52:09 +0530

    programs uploaded

--- ArrayMethods/map.js
+++ ArrayMethods/map.js
@@ -1,3 +1,6 @@
 let arr = [1, 2, 3, 4, 5];
 arr.map((num) => num * 2); // returns a new array with each element multiplied by 2
 console.log(arr); // Output: [1, 2, 3, 4, 5]
+
+const newArr = arr.map((num) => num * 2);
+console.log(newArr); // Output: [2, 4, 6, 8

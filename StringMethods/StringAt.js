f530090cdba3e2df95f58ea2888381f1775e8074
reddirani8496 <reddiranibattuluri@gmail.com>
2025-08-24 22:34:12 +0530

    String at method uploaded

---
+++ StringMethods/StringAt.js
@@ -0,0 +1,5 @@
+let fruitName = "apple";
+console.log(fruitName.at(2)); // p
+console.log(fruitName.at(-1)); // e
+console.log(fruitName.charAt(3)); // l
+console.log(fruitName.charAt(5)); // it will return empty string

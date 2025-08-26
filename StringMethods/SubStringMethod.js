0c2697eae11f5a3520c19c39d11980ec64793d73
reddirani8496 <reddiranibattuluri@gmail.com>
2025-08-26 22:41:15 +0530

    Substring method uploaded

--- StringMethods/SubStringMethod.js
+++ StringMethods/SubStringMethod.js
@@ -2,3 +2,5 @@ let apple = "apple";
 console.log(apple.substring(1, 4)); // ppl
 console.log(apple.substring(2)); // ple
 console.log(apple.substring(-3)); // apple
+console.log(apple.substring(1, -3)); // apple
+console.log(apple.substring(4, 1)); // ppl

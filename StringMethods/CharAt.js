7e32ad25a66d6b5de8d36e789a5f5310c7d4c8f3
reddirani8496 <reddiranibattuluri@gmail.com>
2025-08-28 22:01:55 +0530

    Char at method uploaded

---
+++ StringMethods/CharAt.js
@@ -0,0 +1,6 @@
+let fruit = "apple";
+console.log(fruit.charAt(2)); // Output: "p"
+console.log(fruit.charAt(3)); // Output: "l"
+console.log(fruit.charAt(4)); // Output: "e"
+console.log(fruit.charAt(5)); // Output: "" (empty string, as index 5 is out of bounds)
+console.log(fruit.charAt(-1)); // Output: "" (empty string, as negative index is out of bounds)

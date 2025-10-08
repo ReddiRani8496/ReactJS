798621e308742591adda0259e6ca162cb4d28952
reddirani8496 <reddiranibattuluri@gmail.com>
2025-10-08 20:44:22 +0530

    Power of 4 program uploaded

---
+++ InterviewPractice/PowerOf4.js
@@ -0,0 +1,7 @@
+function isPowerOfFour(n) {
+  // your function implementation
+  if (typeof n != "number" || n < 0 || !Number.isInteger(n)) return false;
+  return Math.log2(n) % 2 == 0;
+}
+
+module.exports = { isPowerOfFour };

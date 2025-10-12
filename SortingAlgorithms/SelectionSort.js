fb597f3c0e9120e9435311c48970f80cd6f2fdf4
reddirani8496 <reddiranibattuluri@gmail.com>
2025-10-12 22:36:38 +0530

    selection sort program uploaded

---
+++ SortingAlgorithms/SelectionSort.js
@@ -0,0 +1,16 @@
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]];
+  }
+
+  return arr;
+};
+
+console.log(selectionSort([5, 1, 4, 2, 8]));

ce9e260a0a5577bf8999813dc3f47254a93d1e63
reddirani8496 <reddiranibattuluri@gmail.com>
2025-10-27 22:33:41 +0530

    Object Example uploaded

---
+++ Functions/ObjectEx.js
@@ -0,0 +1,17 @@
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+};
+
+console.log(
+  "First name: " +
+    person.firstName +
+    ", lastname: " +
+    person.lastName +
+    ", age: " +
+    person.age +
+    ", eyecolor: " +
+    person.eyeColor
+);

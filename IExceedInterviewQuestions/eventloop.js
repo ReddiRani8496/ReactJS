2efefaf859a85f0256f406d3d56180f07b0ee863
reddirani8496 <reddiranibattuluri@gmail.com>
2026-03-08 23:03:30 +0530

    event loop uploaded

--- IExceedInterviewQuestions/eventloop.js
+++ IExceedInterviewQuestions/eventloop.js
@@ -3,3 +3,6 @@ setTimeout(() => {
   console.log("inside time out");
 }, 100);
 console.log("After time out");
+
+// Event Loop: When an browser api is getting called like click of button, timeout, it will be stored in call back queue after call stack
+// is empty event loop moves the call back queue into the call stack.

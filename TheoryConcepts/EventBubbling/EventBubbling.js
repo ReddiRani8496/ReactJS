ab316178aca86a292d91aef5f94dea724a7805a5
reddirani8496 <reddiranibattuluri@gmail.com>
2026-03-11 23:04:19 +0530

    Event bubbling program uploaded

---
+++ TheoryConcepts/EventBubbling/EventBubbling.js
@@ -0,0 +1,11 @@
+document.querySelector("#grandparent").addEventListener("click", () => {
+  console.log("grand parent clicked");
+});
+
+document.querySelector("#parent").addEventListener("click", () => {
+  console.log("parent clicked");
+});
+
+document.querySelector("#child").addEventListener("click", () => {
+  console.log("child clicked");
+});

159f5dfaaa70838876b94946991bae45a37e1991
reddirani8496 <reddiranibattuluri@gmail.com>
2026-08-10 20:36:17 +0530

    URL Validator program uploaded

---
+++ ReactInterviewQuestions/URLValidator.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function URLValidator() {
+  const [input, setInput] = useState("");
+  const [isValid, setIsValid] = useState(null);
+
+  function validateUrl() {
+    // Write logic to validate the url
+  }
+
+  return (
+    <div>
+      <h1>URL Validator</h1>
+
+      <div className="container">{/* Add Input and Result */}</div>
+    </div>
+  );
+}
+export default URLValidator;
